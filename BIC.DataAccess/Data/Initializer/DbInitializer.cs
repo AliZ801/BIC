2611aeaa525796b4ceccd60881890a242496dd8c
Ali Zubair <ali.zubair98@outlook.com>
2020-06-11 15:19:55 +0300

    Comit 2

---
+++ BIC.DataAccess/Data/Initializer/DbInitializer.cs
@@ -0,0 +1,61 @@
+﻿using BIC.Models;
+using BIC.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BIC.DataAccess.Data.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(
+            ApplicationDbContext db,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void Initializer()
+        {
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }catch(Exception ex)
+            {
+
+            }
+
+            if (_db.Roles.Any(r => r.Name == SD.Admin)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(SD.Admin)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.User)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "ali@admin.com",
+                Name = "Ali Zubair",
+                Email = "ali@admin.com",
+                EmailConfirmed = true,
+                PhoneNumber = "9876543210",
+                Address = "Ali Address",
+                City = "Ali City"
+            }, "Ali123!").GetAwaiter().GetResult();
+
+            ApplicationUser user = _db.ApplicationUser.Where(u => u.Email == "ali@admin.com").FirstOrDefault();
+            _userManager.AddToRoleAsync(user, SD.Admin).GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ BIC.DataAccess/Data/Initializer/IDbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BIC.DataAccess.Data.Initializer
+{
+    public interface IDbInitializer
+    {
+        void Initializer();
+    }
+}
